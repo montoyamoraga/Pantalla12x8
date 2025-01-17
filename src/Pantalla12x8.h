d9ede7f94a09d64d50cb2399ef1d63363fc0a7ff
montoyamoraga <montoyamoraga@gmail.com>
2025-01-17 16:27:37 -0300

    agregar dimension a arrays

--- src/Pantalla12x8.h
+++ src/Pantalla12x8.h
@@ -19,16 +19,16 @@ class Pantalla12x8 {
 
 
     // numbers, centered
-    static const uint32_t cero[][];
-    static const uint32_t uno[][];
-    static const uint32_t dos[][];
-    static const uint32_t tres[][];
-    static const uint32_t cuatro[][];
-    static const uint32_t cinco[][];
-    static const uint32_t seis[][];
-    static const uint32_t siete[][];
-    static const uint32_t ocho[][];
-    static const uint32_t nueve[][];
+    static const uint32_t cero[][12];
+    static const uint32_t uno[][12];
+    static const uint32_t dos[][12];
+    static const uint32_t tres[][12];
+    static const uint32_t cuatro[][12];
+    static const uint32_t cinco[][12];
+    static const uint32_t seis[][12];
+    static const uint32_t siete[][12];
+    static const uint32_t ocho[][12];
+    static const uint32_t nueve[][12];
     // static const uint32_t cero[8][12];
     // static const uint32_t uno[8][12];
     // static const uint32_t dos[8][12];
