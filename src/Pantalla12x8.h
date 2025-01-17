706a9d6e5e13e2cc90ff3e8d44f6585deac5eb97
montoyamoraga <montoyamoraga@gmail.com>
2025-01-17 16:25:08 -0300

    tratar de arreglar arrrays con numeros

--- src/Pantalla12x8.h
+++ src/Pantalla12x8.h
@@ -19,16 +19,26 @@ class Pantalla12x8 {
 
 
     // numbers, centered
-    static uint32_t cero[8][12];
-    static uint32_t uno[8][12];
-    static uint32_t dos[8][12];
-    static uint32_t tres[8][12];
-    static uint32_t cuatro[8][12];
-    static uint32_t cinco[8][12];
-    static uint32_t seis[8][12];
-    static uint32_t siete[8][12];
-    static uint32_t ocho[8][12];
-    static uint32_t nueve[8][12];
+    static const uint32_t cero[][];
+    static const uint32_t uno[][];
+    static const uint32_t dos[][];
+    static const uint32_t tres[][];
+    static const uint32_t cuatro[][];
+    static const uint32_t cinco[][];
+    static const uint32_t seis[][];
+    static const uint32_t siete[][];
+    static const uint32_t ocho[][];
+    static const uint32_t nueve[][];
+    // static const uint32_t cero[8][12];
+    // static const uint32_t uno[8][12];
+    // static const uint32_t dos[8][12];
+    // static const uint32_t tres[8][12];
+    // static const uint32_t cuatro[8][12];
+    // static const uint32_t cinco[8][12];
+    // static const uint32_t seis[8][12];
+    // static const uint32_t siete[8][12];
+    // static const uint32_t ocho[8][12];
+    // static const uint32_t nueve[8][12];
 
 };
 
